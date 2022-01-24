cad0a3f4aa3e669a07a317a1d9469d838f398f0c
bvollner <64236358+bvollner@users.noreply.github.com>
2022-01-24 12:41:55 -0800

    Snowman_v2.0

--- Week1/snowman_ex/snowman.js
+++ Week1/snowman_ex/snowman.js
@@ -1,16 +1,18 @@
 let grid = undefined;
 let stroke1 = prompt("Enter a basic color in lowercase letters", "blue");
 let stroke2 = prompt("Enter a second basic color", "gray");
+let stroke3 = prompt("Enter a third color", "orange");
+let stroke4 = prompt("Enter a fourth color", "black");
 function setup() {
     createCanvas(1000, 800);
-    background("#ccc");
-    grid = loadImage("images/100px_grid.png"); 
+    grid = loadImage("images/100px_grid.png");
+    background("#0d6da4");   
 }
 function draw() {
     background(grid);
     //snowman legs//
     fill("#f1f1f1");
-    strokeWeight(20);
+    strokeWeight(10);
     stroke(stroke1);
     //left leg//
     ellipse(350, 650, 200);
@@ -25,14 +27,38 @@ function draw() {
     strokeWeight(40);
     line(400, 120, 600, 120);
     //hat body//
+    fill("gray");
     quad(400, 50, 600, 50, 550, 120, 450, 120);
     //eyes//
-    stroke(0);
+    stroke(stroke4);
     strokeWeight(50);
-    point(425, 200);
-    point(575, 200);
+    point(450, 200);
+    point(550, 200);
     //nose//
-    nofill();
+    fill("orange");
     strokeWeight(10);
-    arc(500, 200, 80, 80, 0,);
+    stroke(stroke3);
+    triangle(495, 250, 505, 250, 500, 275); 
+    //buttons//
+    stroke("gray");
+    strokeWeight(50);
+    point(500, 350);
+    point(500, 450);
+    point(500, 550);
+    //left arm//
+    stroke("#291b01");
+    strokeWeight(10);
+    line(350, 400, 150, 250);
+    line(200, 290, 200, 250);
+    line(200, 290, 180, 290);
+    //right arm//
+    stroke("#291b01");
+    strokeWeight(10);
+    line(675, 400, 800, 250);
+    line(775, 290, 775, 250);
+    line(775, 290, 800, 290);
+    //Ground//
+    fill("#fff");
+    noStroke();
+    quad(0, 800, 0, 720, 1000, 720, 1000, 800); 
 }
