2162621f87edeeec4cfbeefcba9ab1615e0eeb9d
bvollner <64236358+bvollner@users.noreply.github.com>
2022-01-24 08:46:03 -0800

    1.24 push for Prof

---
+++ Week1/snowman_ex/snowman.js
@@ -0,0 +1,11 @@
+let grid = undefined;
+function setup() {
+    createCanvas(1000, 800);
+    background("#ccc");
+    grid = loadImage("snowman_ex/images/100px_grid.png"); 
+}
+function draw() {
+    background(grid);
+    //leg shapes
+    fill("#f1f1f1")
+}
