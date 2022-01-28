ba87acdd3d055049420ff92051a3e79a94ea79d9
bvollner <64236358+bvollner@users.noreply.github.com>
2022-01-27 20:33:06 -0800

    Ex2.1_added

---
+++ Week2/ex2.1/movieArray.js
@@ -0,0 +1,4 @@
+const movieArr = ["The Big Lebowski", "Snatch", "Lock, Stock, and Two Smoking Barrels", "The Royal Tenenbaums", "High Fidelity", "Grosse Pointe Blank"];
+const title = window.prompt("What is your favorite movie?");
+movieArr.push = (title);
+console.log(movieArr);
