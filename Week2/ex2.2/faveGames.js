331711643c2725e27e9b2c215abfbe263fb1aa84
bvollner <64236358+bvollner@users.noreply.github.com>
2022-01-27 22:08:39 -0800

    ex_2.2

---
+++ Week2/ex2.2/faveGames.js
@@ -0,0 +1,27 @@
+const gamesArr = [
+    {title: "Cards Against Humanity",
+    players: "4+",
+    rating: "awkward",
+    description: "You will find out just how depraved and warped your friends and loved ones really are!"
+},
+{
+    title: "Secret Hitler",
+    players: "5-10",
+    rating: "somewhat creepy",
+    description: "Who doesn't love a little covert fascism between friends? Find out just how well your friends can lie!"
+},
+{
+    title: "Checkers",
+    players: "2",
+    rating: "traditional",
+    description: "A simple game of strategy. It's perfect for grandparents who need to entertain themselves or their grandkids."
+},
+{
+    title: "Scrabble",
+    players: "2-4",
+    rating: "my wife thinks it's infuriating",
+    description: "A turn-based game where players earn points by creating words from lettered tiles. Scores are made from both words and tile placement."
+}
+]
+console.log(gamesArr);
+
