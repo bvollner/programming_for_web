6c63d6da6e54bb586c90bc808ce117e8d53db1a1
bvollner <64236358+bvollner@users.noreply.github.com>
2022-01-29 17:42:22 -0800

    ex2.2

--- Week2/ex2.2/faveGames.js
+++ Week2/ex2.2/faveGames.js
@@ -1,27 +1,28 @@
 const gamesArr = [
     {title: "Cards Against Humanity",
-    players: "4+",
-    rating: "awkward",
+    players: "4+ players",
+    rating: "hilariously awkward card game",
     description: "You will find out just how depraved and warped your friends and loved ones really are!"
 },
 {
     title: "Secret Hitler",
-    players: "5-10",
-    rating: "somewhat creepy",
+    players: "5-10 players",
+    rating: "somewhat creepy game",
     description: "Who doesn't love a little covert fascism between friends? Find out just how well your friends can lie!"
 },
 {
     title: "Checkers",
-    players: "2",
-    rating: "traditional",
-    description: "A simple game of strategy. It's perfect for grandparents who need to entertain themselves or their grandkids."
+    players: "2 players",
+    rating: "really traditional game",
+    description: "It's a simple game of strategy. It's perfect for grandparents who need to entertain themselves or their grandkids."
 },
 {
     title: "Scrabble",
-    players: "2-4",
-    rating: "my wife thinks it's infuriating",
+    players: "2-4 players",
+    rating: "a game that my wife thinks is infuriating",
     description: "A turn-based game where players earn points by creating words from lettered tiles. Scores are made from both words and tile placement."
 }
 ]
-console.log(gamesArr);
-
+console.log("Game explanation",gamesArr);
+const gameNumberPick = window.prompt('Pick a number between 1 and 4, and I will tell you about one of my favorite games.');
+window.alert('You picked ' + gamesArr[gameNumberPick - 1].title + '. That is a ' + gamesArr[gameNumberPick - 1].rating + '. It says on the box that it is for ' + gamesArr[gameNumberPick - 1].players + ', but who am I to judge how you spend your time? ' + gamesArr[gameNumberPick - 1].description)
