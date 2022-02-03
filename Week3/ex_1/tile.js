0b6cffde744219a5683e015dfea016771d99a92a
bvollner <64236358+bvollner@users.noreply.github.com>
2022-02-03 14:43:44 -0800

    ex_1_edited

--- Week3/ex_1/tile.js
+++ Week3/ex_1/tile.js
@@ -3,21 +3,31 @@ function setup() {
 }
 function createTile() {
     translate (0,0);
-    fill("blue"); 
+    fill("#4a0594"); 
     rect(0, 0, 200, 200);
+    fill("#006092");
+    rect(100, 0, 200, 100,);
+    rect(0, 100, 100, 200);
+    // intersecting lines //
     stroke("white");
     strokeWeight(5);
     line(0, 0, 200, 200);
     line(200, 0, 0, 200);
-    noStroke();
-    fill("yellow");
-    circle(100, 50, 50);
-    circle(50, 100, 50);
-    circle(150, 100, 50);
-    circle(100, 150, 50);
+    line(100, 200, 100, 0);
+    line(0,100, 200, 100);
+    // white triangle corners, center square //
+    fill("white");
+    square(75, 75, 50);
+    triangle(175, 0, 200, 25, 200, 0);
+    triangle(175, 200, 200, 175, 200, 200);
+    triangle(0, 175, 25, 200, 0, 200);
+    triangle(0, 0, 25, 0, 0, 25);
+    // center circles //
     noFill();
-    stroke("purple");
-    strokeWeight(5);
+    stroke("#4a0594");
+    strokeWeight(10);
+    circle(100, 100, 200);
+    stroke("#006092");
     circle(100, 100, 100);
 }
 function draw() {
