21d3b4c8f15006f5f3ed0c43422b2a23ac1e9af6
bvollner <64236358+bvollner@users.noreply.github.com>
2022-02-27 20:19:59 -0800

    ex_6a

---
+++ Week6/ex_6a/lib/script.js
@@ -0,0 +1,19 @@
+// instance of the Vue instance: Llama facts//
+new Vue({ 
+    el: "#llamaFacts",
+    data: {
+        message: "Llama Facts",
+        items:[
+        { fact: "The Oregon State University School of Veterinary Medicine has a herd of 30 llamas." },
+        { fact: "In the Andes Mountains of Peru, llama fleece has been shorn and used in textiles for about 6,000 years. Llama wool is light, warm, water-repellent, and free of lanolin." },
+        { fact: "One of the ways llamas communicate is by humming." },
+        { fact: "Llamas are much larger and furrier than African Sideneck Turtles." },
+        { fact: "Additionally, llamas also are much more profitable and provide meat, milk, and wool to their owners. Unlike African Sideneck Turtles which just kinda hang out in their tanks like little mooches." },
+        { fact: "Apparently, llamas make excellent guards for herds of small animals. They are very social and will ‘adopt’ a group of sheep or goats as their own herd. Then they will protect the herd by chasing off coyotes and other predators. This is beyond the scope of the African Sideneck Turtle, which would just let your other lifestock be eaten or traumatized." },
+        { fact: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other. Llamas also kick and neck wrestle each other when agitated... African Sideneck Turtles, on the other hand, will bite for the sheer sadistic joy of inflicting pain on their captors." },
+        { fact: "Llamas live to be about 20 years old. Though some only live for 15 years and others live to be 30 years old. African Sideneck Turtles can live to be 50 years old in captivity. Understand that 20 years can be a long time." },
+        { fact: "According to the records of Aymara mythology (an indigenous nation in the Andes and Altiplano regions of South America), llamas are important animals. They believe that the “Heavenly Llama” drinks water from the ocean and provides the rain from its urine. I have no turtle commentary for this one. " },
+        { fact: "The United States first imported llamas during the late 1800s and placed them in several zoos as an exhibit. Fast forward today, according to the South Central Llama Association, there are more than 150,000 llamas in the United States and Canada. This is an excessive amount of llamas." }
+      ]
+    }
+  })
