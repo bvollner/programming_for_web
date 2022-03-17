1fc966499686550bf92546666f99cf4dafc8608b
bvollner <64236358+bvollner@users.noreply.github.com>
2022-03-16 18:10:43 -0700

    Week8_ex8a

--- Week7/ex_7a/pets.js
+++ Week7/ex_7a/pets.js
@@ -1,44 +1,93 @@
-Vue.createApp({
+var vm = Vue.createApp({
     data() {
       return {
+        newPetObj: {
+            name: '',
+            animal: '',
+            age: '',
+            breed: '',
+            image: ''
+        },
         pets: [
         {
             name: 'Opie',
-            animal: 'dog',
+            animal: 'Dog',
             age: 'Ancient',
-            breed: 'Westside Wonder Mutt'
+            breed: 'Westside Ghost Dingo',
+            image: 'images/opie.png'
         }, {
             name: 'Muggsy',
-            animal: 'dog',
+            animal: 'Dog',
             age: '6 years',
-            breed: 'Lab mix'
+            breed: 'Cuddle Moose / Retriever Mix',
+            image: 'images/muggsy.png'
         }, {
             name: 'Kona',
-            animal: 'dog',
+            animal: 'Dog',
             age: '2 years',
-            breed: 'Chi-weenie'
+            breed: 'Nibbling Gremlin',
+            image: 'images/kona.png'
         }, {
             name: 'Jake, Jake from State Farm',
-            animal: 'turtle',
+            animal: 'Turtle',
             age: '16 years',
-            breed: 'African Sideneck Turtle'
+            breed: 'Bougie Sideneck Turtle',
+            image: 'images/jake.png'
         }, {
             name: 'Biff',
-            animal: 'turtle',
+            animal: 'Turtle',
             age: '16 years',
-            breed: 'African Sideneck Turtle'
+            breed: 'Expandable Mud Turtle',
+            image: 'images/biff.png'
         }, {
             name: 'Spoofer',
-            animal: 'cat',
+            animal: 'Cat',
             age: '11 years',
-            breed: 'Calico House Cat'
+            breed: 'Cloned Calico Lab Cat',
+            image: 'images/spoofer.png'
         }, {
             name: 'Noogie',
-            animal: 'cat',
+            animal: 'Cat',
             age: '11 years',
-            breed: 'Calico House Cat'
+            breed: 'Cloned Calico Lab Cat',
+            image: 'images/noogie.png'
+        },
+        {
+            name: 'Yours Truly',
+            animal: 'Human',
+            age: '41 years',
+            breed: 'Midwestern Bearded CouchDweller',
+            image: 'images/yourstruly.png'
+        },
+        {
+            name: 'The Teenager',
+            animal: 'Human',
+            age: '19 years',
+            breed: 'Western Nocturnal Giggler',
+            image: 'images/thekid.png'
         }
         ]
-    }
-    }
+    };
+},
+    methods: {
+        submitHandler: () => {
+            console.log('submitted');
+            vm.pets = vm.pets.concat(vm.newPetObj);
+            vm.resetForm();
+        },
+        resetForm: () => {
+            vm.newPetObj = {
+                name: '',
+                animal: '',
+                age: '',
+                breed: '',
+                image: ''
+            };
+        },
+        deleteItem: item => {
+            vm.pets = vm.pets.filter(pet => {
+                return pet !== item;
+            });
+        }
+    }    
 }).mount('#petsApp');
